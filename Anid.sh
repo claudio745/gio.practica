ce195924642b03e97de8de489df1c9c105b7f95b
claudio afaro <74275767+claudio745@users.noreply.github.com>
2025-04-24 20:47:35 -0400

    Add files via upload

---
+++ Anid.sh
@@ -0,0 +1,8 @@
+set -e
+
+PYTHON=/usr/bin/python3
+
+SCRIPT_PATH="/home/pei/Desktop/Pei/Anid.py"
+
+$PYTHON "$SCRIPT_PATH"
+
---
+++ gore_valparaiso.sh
@@ -0,0 +1,7 @@
+set -e
+
+PYTHON=/usr/bin/python3
+
+SCRIPT_PATH="/home/pei/Desktop/Pei/fondo_gore.py"
+
+$PYTHON "$SCRIPT_PATH"
