abc85da9be52765404356a6900393bd16e914d28
claudio afaro <74275767+claudio745@users.noreply.github.com>
2025-04-29 17:40:38 -0400

    a
    a

--- js/dataloader.js
+++ js/dataloader.js
@@ -1,30 +1,29 @@
 import { renderers } from './renderers.js';
 
 function cargarDatosScraper(url, contenedorId, callback) {
-    const container = document.getElementById(contenedorId);
-    
-    if (!container) {
-      return;
-    }
+  const container = document.getElementById(contenedorId);
   
-    // Mostrar spinner
-    container.innerHTML = '<div class="spinner">Cargando...</div>';
-  
-    fetch(url + '?_=' + new Date().getTime())
-      .then(response => response.json())
-      .then(data => {
-        container.innerHTML = ''; // Limpiar contenido previo
-        data.forEach(item => {
-          const card = callback(item);
-          container.appendChild(card);
-        });
-      })
-      .catch(error => {
-        console.error('Error al cargar datos:', error);
-        container.innerHTML = '<div class="error">Error al cargar los datos.</div>';
-      });
+  if (!container) {
+    return;
   }
-  
+
+  // Mostrar spinner
+  container.innerHTML = '<div class="spinner">Cargando...</div>';
+
+  fetch(url + '?_=' + new Date().getTime()) // para evitar cache
+    .then(response => response.json())
+    .then(data => {
+      container.innerHTML = ''; // Limpiar contenido previo
+      data.forEach(item => {
+        const card = callback(item);
+        container.appendChild(card);
+      });
+    })
+    .catch(error => {
+      console.error('Error al cargar datos:', error);
+      container.innerHTML = '<div class="error">Error al cargar los datos.</div>';
+    });
+}
 
 const fuentes = [
   ['json/fondos_gob.json', 'scraper1-data', renderers.scraper1],
@@ -37,8 +36,17 @@ export function cargarTodosLosDatos() {
   fuentes.forEach(([url, contenedorId, renderer]) => {
     cargarDatosScraper(url, contenedorId, renderer);
   });
+}
 
 
-};
+cargarTodosLosDatos();
 
+
+setInterval(() => {
+  const scrollY = window.scrollY;
+  cargarTodosLosDatos();
+  setTimeout(() => {
+    window.scrollTo(0, scrollY);
+  }, 100); 
+}, 30000);  //30 segundos pa probar 
 
